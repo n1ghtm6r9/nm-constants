983d50d9b2dedc3c09c8391fd848a1898889faf8
Bogdan Sklyarov <n1ghtm6r9@gmail.com>
2024-03-28 17:41:02 +0200

    feat(): add regex constants

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from './dayMs';
 export * from './authKey';
 export * from './defaultHost';
 export * from './defaultBootMode';
+export * from './emailRegex';
+export * from './passwordRegex';
+export * from './usernameRegex';
---
+++ src/usernameRegex.ts
@@ -0,0 +1,1 @@
+export const usernameRegex = /^[A-Za-z][A-Za-z0-9_]{3,32}$/;
